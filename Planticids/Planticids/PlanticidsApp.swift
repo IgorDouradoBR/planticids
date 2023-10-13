58233b0e45a89375ffe6c699af8407c98aeb2715
IgorDouradoBR <igor.d@edu.pucrs.br>
2023-10-13 05:27:53 -0300

    Projeto criado

---
+++ Planticids/Planticids/PlanticidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanticidsApp.swift
+//  Planticids
+//
+//  Created by Igor Dourado  on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanticidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
