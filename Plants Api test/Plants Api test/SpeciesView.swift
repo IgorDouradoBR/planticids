8ffa12edaf66086f860db71ab1ab205ce00657bc
Raquel Ramos <rdsr1304@gmail.com>
2023-10-14 15:10:20 -0300

    "changed: - created the API call for an specific species - Now there is a list of the flowers in the first page!
    What will be changed:
    - Pagination is next
    - integration with another branch?
    - think which details will be more useful when making the app"

---
+++ Plants Api test/Plants Api test/SpeciesView.swift
@@ -0,0 +1,18 @@
+//
+//  SpeciesView.swift
+//  Plants Api test
+//
+//  Created by Raquel Ramos on 14/10/23.
+//
+
+import SwiftUI
+
+struct SpeciesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeciesView()
+}
